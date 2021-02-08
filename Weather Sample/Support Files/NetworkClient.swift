5c3f73c08ae5a3f8afad7da66815be6967fc26dd
Dmitrii Zverev <dmitrii@whitecoat.com.au>
2021-02-08 17:39:20 +1100

    >

--- Weather Sample/Support Files/NetworkClient.swift
+++ Weather Sample/Support Files/NetworkClient.swift
@@ -9,9 +9,10 @@ import Foundation
 struct NetworkClient {
     
     private static let baseURL = "https://api.openweathermap.org/data/2.5/weather"
-    private static let appID = "6fb230ebd5acaa6946bf6d09830d27fc"
+    private static let appID = "" //ENTER HERE
     
     static func getWeather(city: City, complition:  @escaping(Result<WeatherData, Error>)->()) {
+        if appID.isEmpty { fatalError("enter AppID")}
         guard let url = URL(string: "\(baseURL)?id=\(city.id)&units=metric&APPID=\(appID)") else {
             let manualError = NSError(domain: baseURL, code:  0, userInfo: [NSLocalizedDescriptionKey: "Cant create URL"])
             DispatchQueue.main.async { complition(.failure(manualError)) }
